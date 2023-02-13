eada166bab5e6691533bd532e5effafab2a211cd
Andrea Boscolo Camiletto <andrea.camiletto19@gmail.com>
2023-02-14 00:05:44 +0100

    Fix duplicated lines

--- src/main/kotlin/FrameDefaultImpl.kt
+++ src/main/kotlin/FrameDefaultImpl.kt
@@ -1,5 +1,3 @@
-import java.util.ArrayList
-
 
 class FrameDefaultImpl(vararg line: String): Frame{
 
@@ -11,28 +9,19 @@ class FrameDefaultImpl(vararg line: String): Frame{
 
     override fun atLeftOf(other: Frame): Frame {
         if(Integer.parseInt(other.lines[2]) > Integer.parseInt(lines[2])){
-            return FrameDefaultImpl("\n")
+            return FrameDefaultImpl("\n", lines[0], lines[1], lines[2], lines[3], "\n")
         }
         return this
-        /*if(Integer.parseInt(other.lines[5]) < Integer.parseInt(lines[5])) {
-            return FrameDefaultImpl(lines[0], lines[1], lines[2], lines[3], lines[4], "", "", "\n")
-        }
-        return FrameDefaultImpl(lines[0], lines[1], lines[2], lines[3], lines[4], " ")*/
     }
 
     override fun onTopOf(other: Frame): Frame {
         if(Integer.parseInt(other.lines[3]) > Integer.parseInt(lines[3])){
-            return FrameDefaultImpl(" ")
+            return FrameDefaultImpl(" ",lines[0], lines[1], lines[2], lines[3]," ")
         }
         return this
     }
 
     override fun toString(): String {
-        /*var frame = "  \n" +
-                "  ${lines[0]}  \n" +
-                "  ${lines[1]}  \n" +
-                "     \n" +
-                " "*/
         if(lines.size == 1) {
             return lines[0]
         }
@@ -42,43 +31,6 @@ class FrameDefaultImpl(vararg line: String): Frame{
             frame += lines[i]
         }
         return frame
-
-
     }
-
-   /* override fun toString(): String {
-        var frame = " "
-        for (i in 0..4) {
-            frame += lines[0]
-        }
-        frame += "\n"
-        for(i in 0..2){
-            if(lines.size == 3 && i == 0){
-                frame +=lines[1] + "  " + lines[2] +  "  " + lines[1] + "\n"
-            } else if(lines.size > 3){
-                if(i == 0){
-                    frame +=lines[1] + "  " + lines[2] +  "  " + lines[1] + "\n"
-                }else if (i == 1){
-                    frame +=lines[1] + "  " + lines[3] +  "  " + lines[1] + "\n"
-                }else{
-                    frame += lines[1] + "     " + lines[1] + "\n"
-                }
-            } else{
-                frame += lines[1] + "     " + lines[1] + "\n"
-            }
-        }
-        frame += " "
-        for (i in 0..4) {
-            frame += lines[0]
-        }
-        return frame
-    }*/
-    /*override fun toString(): String {
-        var frame = ""
-        for(i in 0..lines.size - 1) {
-            frame += lines[i]
-        }
-        return frame
-    }*/
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,25 +2,28 @@ import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
     //List of elements
-    var el:ElementList = ElementList()
+    val el = ElementList()
 
     //Vertical and horizontal dashed lines
-    val hl: Frame = FrameDefaultImpl(" ----- ")
+    val hl: Frame = FrameDefaultImpl(" -----")
     val vl: Frame = FrameDefaultImpl("|")
 
-
+    val element1 = elementFrame(el.elements[0])
+    val element2 = element1.atLeftOf(elementFrame((el.elements[1])))
+    val element3 = element1.onTopOf(elementFrame(el.elements[2]))
+    println(element1.toString() + element2.toString() + element3.toString())
 
     //Empty Frame
-    val empty = FrameDefaultImpl("       ", "       ", "       ", "       ", "       ")
+    val empty = FrameDefaultImpl("      ", "     ", "     ", "     ", "      ")
 
     //Element box function
     fun elementBox(el: ElementList.Element): Frame = FrameDefaultImpl(
-            hl.toString(),
-            "$vl${centered(el.atomicNumber)}$vl",
-            "$vl${centeredStr(el.symbol)}$vl",
-            "$vl     $vl",
-            hl.toString()
-        )
+        hl.toString(),
+        "$vl${centered(el.atomicNumber)}",
+        "$vl${centeredStr(el.symbol)}",
+        "$vl     ",
+        hl.toString()
+    )
 
     fun elementBoxAfterParticularElements(el: ElementList.Element): Frame = FrameDefaultImpl(
         hl.toString(),
@@ -32,14 +35,14 @@ fun main(args: Array<String>) {
 
     fun elementBoxBeforeParticularElements(el: ElementList.Element): Frame = FrameDefaultImpl(
         hl.toString(),
-        "\\${centered(el.atomicNumber)}$vl",
-        "/${centeredStr(el.symbol)}$vl",
-        "\\     $vl",
+        "\\${centered(el.atomicNumber)}",
+        "/${centeredStr(el.symbol)}",
+        "\\     ",
         hl.toString()
     )
 
     //Group the elements by period
-    var groupByPeriod= ArrayList<ArrayList<ElementList.Element>>()
+    val groupByPeriod= ArrayList<ArrayList<ElementList.Element>>()
     var periodGr = ArrayList<ElementList.Element>()
     var p = 1
     for(i in 0 until el.elements.size){
@@ -58,70 +61,114 @@ fun main(args: Array<String>) {
     //assemble the table
     var line = ""
     var boxesList: ArrayList<Frame> = ArrayList()
-    var j = 1
+    var group = 1
     var period = 0
     var offset = 0
     for(z in groupByPeriod) {
+        //Adding element boxes to list of boxes, elements are ordered by group
         for (elements in groupByPeriod[period]) {
-            if (el.elements[offset].group == j) {
-                if(offset == 56 || offset == 74 || offset == 103 || offset == 117){
+            if (el.elements[offset].group == group) {
+                if(offset == 103 || offset == 117){
                     boxesList.add(elementBoxAfterParticularElements(el.elements[offset]))
-                    j += 1
+                    group += 1
+                    offset += 1
+                } else if(offset == 57 || offset == 75 ){
+                    boxesList.add(elementBoxBeforeParticularElements(el.elements[offset]))
+                    group += 1
                     offset += 1
                 } else {
                     boxesList.add(elementBox(el.elements[offset]))
-                    j += 1
+                    group += 1
                     offset += 1
                 }
             } else {
-                while (el.elements[offset].group != j && j < 19) {
+                while (el.elements[offset].group != group && group < 19) {
                     boxesList.add(empty)
-                    j += 1
+                    group += 1
                 }
                 if(offset == 56 || offset == 74 || offset == 102 || offset == 117){
                     boxesList.add(elementBoxAfterParticularElements(el.elements[offset]))
-                    j += 1
+                    group += 1
                     offset += 1
                 } else if(offset == 90 || offset == 104){
                     boxesList.add(elementBoxBeforeParticularElements(el.elements[offset]))
-                    j += 1
+                    group += 1
                     offset += 1
                 } else {
                     boxesList.add(elementBox(el.elements[offset]))
-                    j += 1
+                    group += 1
                     offset += 1
                 }
             }
         }
         if(period == 7) {
-            line += "\n\n\n\n"
+            line += "\n\n\n"
         }
+
+        //printing the table
+        //Handling the 4th, 5th, 6th and 8th row of the table
         if(period in 4..6 || period > 7) {
             for(i in 1..4){
                 for(j in 0 until boxesList.size) {
-                    line += boxesList[j].lines[i]
+
+                    if(j == 17 && i <= 3) {
+                        line += boxesList[j].lines[i] + "$vl"
+                    } else if(period > 7 && boxesList[j] == empty && i <= 3){
+                        line += boxesList[j].lines[i] + " "
+                    } else {
+                        line += boxesList[j].lines[i]
+                    }
                 }
                 line += "\n"
             }
-        } else if(period in 0..2) {
+        } else if(period in 0..2) { //handling the first two rows
             for(i in 0..3){
                 for(j in 0 until boxesList.size) {
-                    line += boxesList[j].lines[i]
+                    if(j == 0 && boxesList[j+1] == empty) {
+                        if(i == 0) {
+                            line += boxesList[j].lines[i]
+                        }else {
+                            line += boxesList[j].lines[i] + "$vl"
+                        }
+                    }else if(j in 1..15 && boxesList[j] != empty && boxesList[j+1] == empty){
+                        if(i>= 1) {
+                            line +=  boxesList[j].lines[i] + "$vl"
+                        } else {
+                            line += boxesList[j].lines[i]
+                        }
+
+                    }  else if(j in 1..15 && boxesList[j] == empty  && boxesList[j+1] == empty){
+                        if(i == 0) {
+                            line += boxesList[j].lines[i]
+                        } else {
+                            line += boxesList[j].lines[i] + " "
+                        }
+                    } else if (j == 17 && i >= 1){
+                        line += boxesList[j].lines[i] + "$vl"
+                    } else {
+                        line += boxesList[j].lines[i]
+                    }
+
                 }
                 line += "\n"
             }
-        } else {
-
+        } else { //handling the the 3rd and 7th row of the table
             for (i in 0..4) {
                 for (j in 0 until boxesList.size) {
-                    line += boxesList[j].lines[i]
+                    if(j == 17 && period < 6 && i in 1..3) {
+                        line += boxesList[j].lines[i] + "$vl"
+                    } else if (period == 7 && boxesList[j] == empty && i in 1..3){
+                        line += boxesList[j].lines[i] + " "
+                    }else {
+                        line += boxesList[j].lines[i]
+                    }
                 }
                 line += "\n"
             }
         }
 
         period += 1
-        j = 1
+        group = 1
         boxesList.clear()
         boxesList = ArrayList()
         line += ""
@@ -129,6 +176,8 @@ fun main(args: Array<String>) {
     print(line)
 }
 
+
+//Functions to center the atomic number and the name of the element
 fun centered(num: Int): String {
     val t = num
     if(t < 10) {
